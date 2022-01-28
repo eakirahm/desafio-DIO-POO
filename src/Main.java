4f5bc6fdf69454c0d0805c0b93cf157e0707d874
eakirahm <eakirahm@gmail.com>
2022-01-28 02:58:46 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -25,6 +27,39 @@ public class Main {
         mentoria.setDescricao("descrição mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devEduardo = new Dev();
+        devEduardo.setNome("Eduardo");
+        devEduardo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Eduardo: " + devEduardo.getConteudosInscritos());
+
+        devEduardo.progredir();
+        devEduardo.progredir();
+
+        System.out.println("Conteúdos inscritos Eduardo: " + devEduardo.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Eduardo: " + devEduardo.getConteudosConcluidos());
+        System.out.println("XP: " + devEduardo.calcularTotalXp());
+
+        System.out.println("----------------------------");
+
+        Dev devCarol = new Dev();
+        devCarol.setNome("Carol");
+        devCarol.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Carol: " + devCarol.getConteudosInscritos());
+
+        devCarol.progredir();
+        devCarol.progredir();
+        devCarol.progredir();
+
+        System.out.println("Conteúdos inscritos Carol: " + devCarol.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Carol: " + devCarol.getConteudosConcluidos());
+        System.out.println("XP: " + devCarol.calcularTotalXp());
+
     }
 }
